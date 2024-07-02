64ed97ebcdb7994996bcab2df625f8e90bc32afc
deanv <Annhi1975@gmail.com>
2024-07-02 14:14:42 -0700

    temp

---
+++ scripts/UI/Resolution.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+var Resolutions : Dictionary = {
+								"2560x1440":Vector2(2560, 1440),
+								"1920x1200":Vector2(1920, 1200),
+								"1920x1080":Vector2(1920, 1080),
+								"1680x1050":Vector2(1680, 1050),
+								"1440x900":Vector2(1440, 900),
+								"1366x768":Vector2(1366, 768),
+								"1280x800":Vector2(1280, 800),
+								"1280x720":Vector2(1280, 720),
+								"1024x768":Vector2(1024, 768),
+								"800x600":Vector2(800, 600),
+								"640x480":Vector2(640, 480)
+							   }
+
